4b256ac7b200657ce56dd4d48f2360e55d3513f5
DioufFLR <geoffreyfleur@gmail.com>
2022-11-11 12:45:13 +0100

    qualifications

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,7 +31,7 @@ function linkAction(){
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 //================ ACCORDION SKILLS ================
-const skillsContent = document.getElementById("skills__content");
+const skillsContent = document.getElementsByClassName("skills__content");
 const skillsHeader = document.querySelectorAll(".skills__header");
 
 function toggleSkills() {
@@ -48,8 +48,26 @@ function toggleSkills() {
 skillsHeader.forEach((el) => {
     el.addEventListener("click", toggleSkills)
 })
-//================ QUALIFICATION TABS ================
 
+//================ QUALIFICATION TABS ================
+const tabs = document.querySelectorAll("[data-target]");
+const tabContents = document.querySelectorAll("[data-content]");
+
+tabs.forEach(tab => {
+    tab.addEventListener("click", () => {
+        const target = document.querySelector(tab.dataset.target);
+
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove("qualification__active");
+        })
+        target.classList.add("qualification__active");
+
+        tabs.forEach(tab => {
+            tab.classList.remove("qualification__active");
+        })
+        tab.classList.add("qualification__active");
+    })
+})
 //================ SERVICES MODAL ================
 
 //================ PORTFOLIO SWIPER ================
